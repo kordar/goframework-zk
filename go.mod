4475f6c669dc7cf1044634cc2e2485532d0b06fb
perpel <572821520@qq.com>
2024-06-19 05:56:47 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kordar/goframework-zk
+
+go 1.16
+
+require github.com/samuel/go-zookeeper v0.0.0-20201211165307-7117e9ea2414
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/samuel/go-zookeeper/zk"
+	"time"
+)
+
+func main() {
+	// 创建zk连接地址
+	hosts := []string{"43.139.223.7:62181"}
+	// 连接zk
+	conn, _, err := zk.Connect(hosts, time.Second*5)
+	defer conn.Close()
+	if err != nil {
+		fmt.Println("eeeeeee", err)
+		return
+	}
+	println(conn.Server())
+}
